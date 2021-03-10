4475089e75849bf3c14f3fe91acfcf58c4644cf5
Thanushree0499 <73820030+Thanushree0499@users.noreply.github.com>
2021-03-10 18:16:07 +0530

    Add files via upload

--- mobile-app/src/main/java/com/xwork/mobile/dao/MobilesDAOImpl.java
+++ mobile-app/src/main/java/com/xwork/mobile/dao/MobilesDAOImpl.java
@@ -17,10 +17,10 @@ public class MobilesDAOImpl implements MobilesDAO {
 	public void saveMobile(MobileDTO mobileDTO) {
 		System.out.println("invoked saveMobile");
 		Session session = null;
-		Transaction transaction=null;
+		Transaction transaction = null;
 		try {
 			SessionFactory sessionFactory = SingleFactory.getSessionFactory();
-			session=sessionFactory.openSession();
+			session = sessionFactory.openSession();
 			transaction = session.beginTransaction();
 			session.save(mobileDTO);
 			transaction.commit();
@@ -32,7 +32,7 @@ public class MobilesDAOImpl implements MobilesDAO {
 				if (Objects.nonNull(session)) {
 					session.close();
 					System.out.println("session is closed");
-				}else {
+				} else {
 					System.out.println("session is not closed");
 				}
 				SingleFactory.closeFactory();
@@ -49,7 +49,7 @@ public class MobilesDAOImpl implements MobilesDAO {
 		Session session = null;
 		try {
 			SessionFactory sessionFactory = SingleFactory.getSessionFactory();
-		    session=sessionFactory.openSession();
+			session = sessionFactory.openSession();
 			MobileDTO mobileDTO = session.load(MobileDTO.class, mobileId);
 			System.out.println(mobileDTO);
 		} catch (HibernateException he) {
@@ -59,14 +59,14 @@ public class MobilesDAOImpl implements MobilesDAO {
 				if (Objects.nonNull(session)) {
 					session.close();
 					System.out.println("session is closed");
-				}else {
+				} else {
 					System.out.println("session is not closed");
 				}
 				SingleFactory.closeFactory();
 			} catch (HibernateException he2) {
 				he2.printStackTrace();
 			}
-			
+
 		}
 	}
 
@@ -74,16 +74,16 @@ public class MobilesDAOImpl implements MobilesDAO {
 	public void updateMobile(int mobileId) {
 		System.out.println("invoked updateMobile");
 		Session session = null;
-		Transaction transaction=null;
+		Transaction transaction = null;
 		try {
 			SessionFactory sessionFactory = SingleFactory.getSessionFactory();
-			session=sessionFactory.openSession();
-		    MobileDTO mobileDTO=session.get(MobileDTO.class,mobileId);
-		    mobileDTO.setBrand("oneplus");
-		    transaction=session.beginTransaction();
-		    session.update(mobileDTO);
-		    transaction.commit();
-		}catch (HibernateException he) {
+			session = sessionFactory.openSession();
+			MobileDTO mobileDTO = session.get(MobileDTO.class, mobileId);
+			mobileDTO.setBrand("oneplus");
+			transaction = session.beginTransaction();
+			session.update(mobileDTO);
+			transaction.commit();
+		} catch (HibernateException he) {
 			transaction.rollback();
 			he.printStackTrace();
 		} finally {
@@ -91,7 +91,7 @@ public class MobilesDAOImpl implements MobilesDAO {
 				if (Objects.nonNull(session)) {
 					session.close();
 					System.out.println("session is closed");
-				}else {
+				} else {
 					System.out.println("session is not closed");
 				}
 				SingleFactory.closeFactory();
@@ -99,37 +99,37 @@ public class MobilesDAOImpl implements MobilesDAO {
 				he2.printStackTrace();
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void deleteMobile(int mobileId) {
 		System.out.println("invoked deleteMobile");
 		Session session = null;
-		Transaction transaction=null;
+		Transaction transaction = null;
 		try {
 			SessionFactory sessionFactory = SingleFactory.getSessionFactory();
-			session=sessionFactory.openSession();
-		    MobileDTO mobileDTO = session.get(MobileDTO.class, mobileId);
-		    transaction = session.beginTransaction();
-		    session.delete(mobileDTO);
-		    transaction.commit();
-	   }catch (HibernateException he) {
-		   transaction.rollback();
-		he.printStackTrace();
-	   } finally {
-		   try {
+			session = sessionFactory.openSession();
+			MobileDTO mobileDTO = session.get(MobileDTO.class, mobileId);
+			transaction = session.beginTransaction();
+			session.delete(mobileDTO);
+			transaction.commit();
+		} catch (HibernateException he) {
+			transaction.rollback();
+			he.printStackTrace();
+		} finally {
+			try {
 				if (Objects.nonNull(session)) {
 					session.close();
 					System.out.println("session is closed");
-				}else {
+				} else {
 					System.out.println("session is not closed");
 				}
 				SingleFactory.closeFactory();
 			} catch (HibernateException he2) {
 				he2.printStackTrace();
 			}
-	}
+		}
 
-}
+	}
 }
